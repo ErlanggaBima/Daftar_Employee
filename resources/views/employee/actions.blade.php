d2fa81eaac1b7f3c423fa995fefa0cb07c88cd0a
Erlangga Bima <erlanggabima347@gmail.com>
2024-06-23 21:14:48 +0700

    pembenaran

--- resources/views/employee/actions.blade.php
+++ resources/views/employee/actions.blade.php
@@ -6,11 +6,11 @@
     <a href="{{ route('employees.edit', ['employee' => $employee->id]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
 
     <div>
-        <form action="{{ route('employees.destroy', ['employee' => $employee->id]) }}" method="POST">
+        {{-- <form action="{{ route('employees.destroy', ['employee' => $employee->id]) }}" method="POST">
             @csrf
-            @method('delete')
+            @method('delete') --}}
             <button type="submit" class="btn btn-outline-dark btn-sm me-2 deleteBtn" data-id="{{ $employee->id }}"><i class="bi-trash"></i></button>
-        </form>
+        {{-- </form> --}}
     </div>
 </div>
 
