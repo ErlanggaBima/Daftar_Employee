5fd0e2879fca2708bdbe8f5770852e0afbdce234
Erlangga Bima <erlanggabima347@gmail.com>
2024-06-23 21:19:21 +0700

    insyaallah fix

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -86,11 +86,11 @@
                                     <a href="{{ route('employees.edit', ['employee' => $employee->id]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
 
                                     <div>
-                                        <form action="{{ route('employees.destroy', ['employee' => $employee->id]) }}" method="POST">
+                                        {{-- <form action="{{ route('employees.destroy', ['employee' => $employee->id]) }}" method="POST">
                                             @csrf
-                                            @method('delete')
-                                            <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
-                                        </form>
+                                            @method('delete') --}}
+                                            <button type="submit" class="btn btn-outline-dark btn-sm me-2 deleteBtn" data-id="{{ $employee->id }}"><i class="bi-trash"></i></button>
+                                        {{-- </form> --}}
                                     </div>
 
                                 </div>
@@ -119,6 +119,58 @@
             })
         </script>
     @endif
+    <script>
+        $(document).ready(function() {
+            $(".deleteBtn").click(function(){
+                var id = $(this).data('id');
+                    Swal.fire({
+                        title : 'Apakah Anda Yakin ?',
+                        text : "Anda tidak akan dapat mengembalikan ini!",
+                        icon : 'warning',
+                        showCancelButton : true,
+                        confirmButtonColor : '#d33',
+                        cancelButtonColor : '#3085d6',
+                        confirmButtonText : "Ya, Hapus Data!"
+                    }).then((result) => {
+                        if(result.isConfirmed){
+                           $.ajax({
+                            type: "POST",
+                            url: "{{ route('data.destroy') }}",
+                            data: {
+                                _token: "{{ csrf_token() }}",
+                                id : id
+                            },
+                            success : function(response) {
+                                Swal.fire({ 
+                                    title : "Sukses !",
+                                    text : "Data Berhasil Dihapus!",
+                                    icon : "success"
+                                }).then((result) => {
+                                    location.reload();
+                                })
+                            },
+                            error : function(error) {
+                                Swal.fire({
+                                    icon : 'error',
+                                    title : 'Oops...',
+                                    text : "Terjadi Kesalahan!"
+                                })
+                            }
+                           })
+                        } else if (result.dismiss === Swal.DismissReason.cancel) {
+                            Swal.fire (
+                                'Batal',
+                                'Tidak ada perubahan yang dilakukan.',
+                                'info'
+                            ).then((result) => {
+                                location.reload();
+                            })
+                        }
+                    }
+                )
+                })
+            })
+    </script>
 
 
 </body>
